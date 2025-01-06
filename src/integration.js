71ed2de3b9d615ce93ebdb6f50e03b5473991e51
Sebastian <pdsccode@gmail.com>
2025-01-06 07:32:51 +0100

    Fixed missing Display update for DSI

--- src/integration.js
+++ src/integration.js
@@ -63,8 +63,10 @@ const init = async (args) => {
       initReboot(INTEGRATION.client);
       initRefresh(INTEGRATION.client);
       initKiosk(INTEGRATION.client);
-      // initDisplay(INTEGRATION.client);  - Commented out for Pi 4 testing purposes as of 05.01.2025 - 16:59
-
+      if (args.display_type.includes("DSI")){
+        initDisplay(INTEGRATION.client);
+      }
+      
       // Init client sensors
       initModel(INTEGRATION.client);
       initSerialNumber(INTEGRATION.client);
@@ -139,7 +141,7 @@ const initShutdown = (client) => {
     .on("message", (topic, message) => {
       if (topic === `${root}/execute`) {
         console.log("Shutdown system...");
-        // hardware.setDisplayStatus("ON"); - Commented out for Pi 4 testing purposes as of 05.01.2025 - 16:59
+        hardware.setDisplayStatus("ON");
         hardware.shutdownSystem();
       }
     })
@@ -168,7 +170,7 @@ const initReboot = (client) => {
     .on("message", (topic, message) => {
       if (topic === `${root}/execute`) {
         console.log("Rebooting system...");
-        // hardware.setDisplayStatus("ON"); - Commented out for Pi 4 testing purposes as of 05.01.2025 - 16:59
+        hardware.setDisplayStatus("ON");
         hardware.rebootSystem();
       }
     })
@@ -197,7 +199,7 @@ const initRefresh = (client) => {
     .on("message", (topic, message) => {
       if (topic === `${root}/execute`) {
         console.log("Refreshing webview...");
-        // hardware.setDisplayStatus("ON"); - Commented out for Pi 4 testing purposes as of 05.01.2025 - 16:59
+        hardware.setDisplayStatus("ON");
         WEBVIEW.view.webContents.reloadIgnoringCache();
       }
     })
@@ -236,7 +238,7 @@ const initKiosk = (client) => {
       if (topic === `${root}/set`) {
         const status = message.toString();
         console.log("Set Kiosk Status:", status);
-        // hardware.setDisplayStatus("ON"); - Commented out for Pi 4 testing purposes as of 05.01.2025 - 16:59
+        hardware.setDisplayStatus("ON");
         switch (status) {
           case "Framed":
             WEBVIEW.window.restore();
